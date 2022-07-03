ce4e5d1ea523ae11792d322874f770b9a8b5a6f0
Matias Bianchi <matiasezequielbian@gmail.com>
2022-07-03 13:42:15 -0300

    Preact removed because of navbar bugs

--- next.config.js
+++ next.config.js
@@ -68,15 +68,6 @@ const config = {
 	},
 	reactStrictMode: true,
 	webpack: (config, { dev, isServer }) => {
-		// Replace React with Preact only in client production build
-		if (!dev && !isServer) {
-			Object.assign(config.resolve.alias, {
-				react: 'preact/compat',
-				'react-dom/test-utils': 'preact/test-utils',
-				'react-dom': 'preact/compat',
-			});
-		}
-
 		config.plugins.push(new WindiCSS());
 
 		config.module.rules.push({
